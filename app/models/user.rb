1348ee592dcd35b2f3dfa5635f0f6ad792158a71
Ahmad Kamran <ahmad.eritheia@gmail.com>
2022-08-29 22:52:20 +0500

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  before_save :email_to_lowercase
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  protected
+  def email_to_lowercase
+    self.email = email.downcase
+  end
 end
